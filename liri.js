0b55ea47121048db741bf78daffa556367e0c033
JD <jdfili14@gmail.com>
2019-01-17 23:35:37 -0800

    did 'do-what-it-says'

--- liri.js
+++ liri.js
@@ -7,19 +7,22 @@ var fs = require('fs');
 var spotify = new spotify(keys.spotify);
 var omdb = keys.omdb.key;
 var bit = keys.BandsInTown.key;
-var action = process.argv[2];
-var search = process.argv.slice(3).join(" ");
 
-function concertThis() {
+function initialize() {
+    var action = process.argv[2];
+    var search = process.argv.slice(3).join(" ");
+    toDo(action, search);
+}
+
+function concertThis(search) {
 
     axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=" + bit)
         .then(
             function (response) {
                 for (var i = 0; i < response.data.length; i++) {
-                    console.log("Venue: " + response.data[i].venue.name);
-                    console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
-                    console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                    console.log("___________________________________")
+                    console.log("Venue: " + response.data[i].venue.name + "\nLocation: " + response.data[i].venue.city + ", " +
+                        response.data[i].venue.country + "\nDate: " + moment(response.data[i].datetime).format("MM/DD/YYYY") +
+                        "\n----------------------------");
                 }
             })
         .catch(function (error) {
@@ -27,7 +30,7 @@ function concertThis() {
         });
 };
 
-function spotifyThisSong() {
+function spotifyThisSong(search) {
 
     spotify.search({
         type: 'track',
@@ -37,30 +40,21 @@ function spotifyThisSong() {
             return console.log('Error occurred: ' + err);
         }
 
-        for (var i = 0; i < 10; i++) {
-            var results = data.tracks.items;
-            console.log(results[i].artists[0].name);
-            console.log(results[i].name);
-            console.log(results[i].href);
-            console.log(results[i].album.name);
-            console.log("___________________________")
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            var response = data.tracks.items;
+            console.log(response[i].artists[0].name + "\n" + response[i].name + "\n" + response[i].href +
+                "\n" + response[i].album.name + "\n---------------------");
         }
     });
 }
 
-function movieThis() {
+function movieThis(search) {
     axios.get('http://www.omdbapi.com/?apikey=' + omdb + '&t=' + search)
         .then(function (response) {
             var info = response.data;
-            console.log("Title: " + info.Title);
-            console.log("Year: " + info.Year);
-            console.log("IMDB Rating: " + info.imdbRating);
-            console.log("RottenTomatoes Rating: " + info.Ratings[1].Value);
-            console.log("Country: " + info.Country);
-            console.log("Language: " + info.Language);
-            console.log("Plot: " + info.Plot);
-            console.log("Actors: " + info.Actors);
-
+            console.log("Title: " + info.Title + "\nYear: " + info.Year + "\nIMDB Rating: " + info.imdbRating +
+                "\nRottenTomatoes Rating: " + info.Ratings[1].Value + "\nCountry: " + info.Country + "\nPlot: " + info.Plot +
+                "\nActors: " + info.Actors);
         })
         .catch(function (error) {
             console.log(error);
@@ -72,25 +66,33 @@ function doWhatItSays() {
         if (error) {
             return console.log(error);
         }
-        console.log(data);
+
+        var dataArr = data.split(",")
+        var doAction = dataArr[0];
+        var doSearch = dataArr[1];
+        toDo(doAction, doSearch);
     })
 }
 
-switch (action) {
-    case "concert-this":
-        concertThis();
-        break;
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
-    case "movie-this":
-        movieThis();
-        break;
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
-    default: 
-    {
-        console.log("error: invalid request")
-    }    
+function toDo(action, search) {
+    switch (action) {
+        case "concert-this":
+            concertThis(search);
+            break;
+        case "spotify-this-song":
+            spotifyThisSong(search);
+            break;
+        case "movie-this":
+            movieThis(search);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            {
+                console.log("error: invalid request")
+            }
+    };
 };
+
+initialize();
